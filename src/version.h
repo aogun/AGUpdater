fa830a2b298d7df08a060c8d2adbf7ab39ea3ff1
aogun <angus_ag@msn.com>
2026-03-18 02:39:11 -0400

    fix: 链接 crypt32 库（cpp-httplib SSLClient 加载 Windows 证书库需要）

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define APP_VERSION_MAJOR 0
 #define APP_VERSION_MINOR 1
-#define APP_VERSION_PATCH 6
+#define APP_VERSION_PATCH 7
 
 #define AG_STRINGIFY_(x) #x
 #define AG_STRINGIFY(x) AG_STRINGIFY_(x)
